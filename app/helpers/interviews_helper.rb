6349df92a5390894e8679d488e2f2734bde9e85d
theforestvn88 <velainuirung@gmail.com>
2022-01-26 23:31:02 +0700

    support select code-languages & code-styles

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -1,15 +1,56 @@
 module InterviewsHelper
-  def interview_stream(*interviews, **attributes, &block)
+  SUPPORT_LANGS = [
+    "ruby",
+    "python",
+    "javascript", 
+    "typescript",
+    "go",
+    "java",
+    "kotlin",
+    "dart",
+    "swift",
+    "objectivec",
+    "c", 
+    "cpp",
+    "csharp",
+    "rust",
+    "php",
+    "perl",
+    "scala",
+    "erlang",
+    "elixir",
+    "crystal",
+    "clojure",
+    "css",
+    "scss",
+    "shell",
+    "bash",
+    "dockerfile",
+    "sql",
+    "r",
+    "julia"
+  ].freeze
+
+  SUPPORT_STYLES = [
+    "default", 
+    "github"
+  ].freeze
+
+  def interview_stream(*interviews, **attributes)
     attributes[:channel] = attributes[:channel]&.to_s || "InterviewStreamsChannel"
     attributes[:"signed-stream-name"] = Turbo::StreamsChannel.signed_stream_name(interviews)
     attributes[:"interview-id"] = interviews.first.id
     attributes[:"user"] = SecureRandom.hex(6) # TODO: replace by User model
 
     tag.interview_stream(**attributes) do |tag|
-      tag.div(class: "code-editor") do |tag|
-        tag.pre("# in code we trust !!!", class: "code-hl")
-          .concat tag.textarea(class: "input-transparent", spellcheck: "false")
-      end
+      tag.label("Lang: ").concat select_tag('lang', options_for_select(SUPPORT_LANGS, "ruby")).concat(
+        tag.label("   Style: ").concat select_tag('style', options_for_select(SUPPORT_STYLES, "default")).concat(
+          tag.div(class: "code-editor") { |tag|
+            tag.pre("# in code we trust !!!", class: "code-hl")
+              .concat tag.textarea(class: "input-transparent", spellcheck: "false")
+          }
+        )
+      )
     end
   end
 end
--- app/javascript/interview/interview_element.js
+++ app/javascript/interview/interview_element.js
@@ -8,12 +8,27 @@ class InterviewElement extends HTMLElement {
       received: this.dispatchMessageEvent.bind(this)
     });
 
+    let langSelect = this.querySelector("#lang");
+    langSelect.addEventListener('change', (e) => {
+      this.lang = e.target.value;
+      highlightCode(codeEditor, this.lang);
+    });
+
+    let styleSelect = this.querySelector("#style");
+    styleSelect.addEventListener('change', (e) => {
+      this.style = e.target.value;
+      for (let link of document.querySelectorAll(".codestyle")) {
+        link.disabled = !link.href.match(this.style + "\\.min.css$");
+      }
+      highlightCode(codeEditor, this.lang);
+    });
+
     let codeInput = this.querySelector("textarea");
     let codeEditor = this.querySelector(".code-editor");
     let codeHighlight = this.querySelector(".code-hl");
 
     codeInput.value = codeHighlight.firstChild.textContent;
-    highlightCode(codeEditor, "ruby");
+    highlightCode(codeEditor, this.lang);
     
     codeInput.addEventListener('input', (e) => {
       let codeText = e.target.value;
@@ -23,7 +38,7 @@ class InterviewElement extends HTMLElement {
         "user": this.getAttribute("user")
       });
       codeHighlight.firstChild.textContent = codeText;
-      highlightCode(codeEditor, "ruby");
+      highlightCode(codeEditor, this.lang);
     });
   }
 
@@ -39,7 +54,7 @@ class InterviewElement extends HTMLElement {
       let codeEditor = this.querySelector(".code-editor");
       let codeHighlight = this.querySelector(".code-hl");
       codeHighlight.firstChild.textContent = data.code;
-      highlightCode(codeEditor, "ruby", true);
+      highlightCode(codeEditor, this.lang, true);
     }
 
     return this.dispatchEvent(event);
@@ -49,6 +64,30 @@ class InterviewElement extends HTMLElement {
     const channel = this.getAttribute("channel");
     const signed_stream_name = this.getAttribute("signed-stream-name");
     return { channel, signed_stream_name };
+  }
+
+  get lang() {
+    if (!this.selectedLang) {
+      this.selectedLang = this.getAttribute("language") || "ruby";
+    }
+
+    return this.selectedLang;
+  }
+
+  set lang(language) {
+    this.selectedLang = language;
+  }
+
+  get style() {
+    if (!this.selectedStyle) {
+      this.selectedStyle = this.getAttribute("style") || "default";
+    }
+
+    return this.selectedStyle;
+  }
+
+  set style(_style) {
+    this.selectedStyle = _style;
   }
 }
 
