465a88a3d6205d52f274ab8fc0e8cd3fc2d48eba
theforestvn88 <theforestvn88@gmail.com>
2022-03-15 23:06:27 +0700

    user's scheduling service

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,5 +2,18 @@
 
 class Interview < ApplicationRecord
     belongs_to  :interviewer, class_name: "User" # required
-    belongs_to  :candidate, class_name: "User" # allow unregister users, interviewer could use `note` to note candidate info.
+    belongs_to  :candidate, class_name: "User", optional: true # allow unregister users, interviewer could use `note` to note candidate info.
+
+    scope :as_interviewer, ->(interviewer) {
+        where(interviewer_id: interviewer.id)
+    }
+
+    scope :as_candidate, ->(candidate) {
+        where(candidate_id: candidate.id)
+    }
+
+    scope :by_time, ->(from, to) {
+        range = from..to
+        where(start_time: range).or(where(end_time: range))   
+    }
 end
--- test/models/interview_test.rb
+++ test/models/interview_test.rb
@@ -1,13 +1,30 @@
 require "test_helper"
 
 class InterviewTest < ActiveSupport::TestCase
-  test "validate interviewer existed" do
-    user = User.create(id: 1, name: "coder1", email: "xxx@gmail.com")
+  test "create: validate interviewer existed" do
+    user = users(:coder1)
     
     interview1 = Interview.create(candidate: user);
     assert_equal interview1.errors[:interviewer], ["must exist"]
 
     interview2 = Interview.create(interviewer: user)
     assert_equal interview2.errors[:interviewer], []
+  end
+
+  test "query by owner (as interviewer)" do
+    interviewer = users(:coder1)
+    assert_equal Interview.as_interviewer(interviewer), interviews(:ruby_dev1, :ruby_dev2)
+  end
+
+  test "as candidate" do
+    candidate = users(:coder2)
+    assert_equal Interview.as_candidate(candidate), interviews(:ruby_dev1, :ruby_dev2)
+  end
+
+  test "query by time" do
+    from_time = interviews(:ruby_dev1).start_time
+    to_time = interviews(:ruby_dev2).end_time
+    assert_equal Interview.by_time(from_time, to_time), interviews(:ruby_dev1, :ruby_dev2)
+    assert_equal Interview.by_time(to_time, DateTime.now), [interviews(:ruby_dev2)]
   end
 end
