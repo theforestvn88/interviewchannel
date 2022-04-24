8e0b4cbb1d5b8e15610795ca2b8bb97b02aa03dc
theforestvn88 <theforestvn88@gmail.com>
2022-04-24 18:39:27 +0700

    limit input view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    class ActionView::Helpers::FormBuilder
+        def limit_text_area(method, options = {})
+            content = \
+                "<div data-controller='helper' data-helper-limit-value='#{options[:limit]}'>" + 
+                    text_area(method, options.merge({data: { helper_target: "limitInput", action: "keyup->helper#limitCharacters"}})) +
+                    "<span data-helper-target='limitWarning'></span>" +
+                "</div>"
+
+            content.html_safe
+        end
+    end
 end
