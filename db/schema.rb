d71de41e1ea85238932ab521b99a7fc05901a80b
theforestvn88 <theforestvn88@gmail.com>
2022-06-16 20:28:24 +0700

    add unique index assigments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_122811) do
+ActiveRecord::Schema.define(version: 2022_06_16_131334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2022_06_14_122811) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["interview_id", "user_id"], name: "index_assignments_on_interview_id_and_user_id", unique: true
     t.index ["interview_id"], name: "index_assignments_on_interview_id"
     t.index ["user_id"], name: "index_assignments_on_user_id"
   end
