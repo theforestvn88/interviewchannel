c1245ee04f4a998f9839a466d3e313c2b39447e1
theforestvn88 <velainuirung@gmail.com>
2022-06-28 16:02:18 +0700

    disable file attachment on trix

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
+
+
+window.addEventListener("trix-file-accept", function(event) {
+  event.preventDefault()
+  alert("File attachment not supported!")
+})
