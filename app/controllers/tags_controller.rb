4d5297118e3a69cf39ddba7ae0200b3ac1285763
theforestvn88 <velainuirung@gmail.com>
2022-06-28 21:45:42 +0700

    colorize tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -31,7 +31,7 @@ class TagsController < AdminController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to tag_url(@tag), notice: "Tag was successfully created." }
+        format.html { redirect_to tag_url(@tag) }
         format.json { render :show, status: :created, location: @tag }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -44,7 +44,7 @@ class TagsController < AdminController
   def update
     respond_to do |format|
       if @tag.update(tag_params)
-        format.html { redirect_to tag_url(@tag), notice: "Tag was successfully updated." }
+        format.html { redirect_to tag_url(@tag) }
         format.json { render :show, status: :ok, location: @tag }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,7 +58,7 @@ class TagsController < AdminController
     @tag.destroy
 
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: "Tag was successfully destroyed." }
+      format.html { redirect_to tags_url }
       format.json { head :no_content }
     end
   end
