1a06f24a20ab52490e93aebb4d3f89855e9a0cfd
theforestvn88 <velainuirung@gmail.com>
2022-07-17 20:38:24 +0700

    refactor rate limmiter

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -3,28 +3,29 @@
 class RateLimiter
   attr_reader :user, :action, :max, :secs
   
-  def initialize(user, action, max, secs, error_code = nil)
+  def initialize(user, action, max, error_code = nil, **options_cache)
     @user = user
     @action = action
     @max = max
-    @secs = secs
     @error_code = error_code
+    @options_cache = options_cache
 
     @key = build_key
   end
 
   def check!
     return true if @user.admin?
-
-    unless (count = Rails.cache.read(@key)) > 0
-      Rails.cache.write(@key, 0, expires_in: @secs)
+    
+    unless (count = Rails.cache.read(@key).to_i) > 0
+      Rails.cache.write(@key, 0, **@options_cache)
     end
-    raise RateLimiter::LimitExceeded.new(@error_code) if count.to_i > @max
+    raise RateLimiter::LimitExceeded.new(@action, @max, @error_code) if count >= @max
 
-    Rails.cache.write(@key, count + 1)
+    Rails.cache.write(@key, count + 1, **@options_cache)
   end
 
   def reset
+    Rails.cache.delete(@key)
   end
 
   private def build_key
@@ -32,12 +33,18 @@ class RateLimiter
   end
 
   class LimitExceeded < StandardError
-    attr_reader :error_code
+    attr_reader :action, :limit, :error_code
 
-    def initialize(error_code)
+    def initialize(action, limit, error_code)
+      @action = action
+      @limit = limit
       @error_code = error_code
     end
 
-    def error_message; end
+    def error_message
+      actions = action.to_s.split('_')
+      time = error_code.to_s.split('_').first
+      "You #{actions.first} too many #{actions.last} (#{limit} #{actions.last} / #{time})"
+    end
   end
 end
